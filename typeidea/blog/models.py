b61b3d90878adb73808390573147e3ab42a0a4b3
persue <383169612@qq.com>
2019-11-16 20:37:56 +0800

    [typeidea]chapter six code

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -17,6 +17,9 @@ class Category(models.Model):
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     
+    def __str__(self):
+        return self.name
+    
     class Meta:
         verbose_name = verbose_name_plural = "分类"
 
