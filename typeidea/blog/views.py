4bf88767acc2c52e7c006681991698e20b220856
persue <383169612@qq.com>
2019-11-21 23:01:49 +0800

    using class-based view

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,10 +1,64 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
+
 #from django.http import HttpResponse
 from .models import Post, Tag, Category
 from config.models import SideBar
-# Create your views here.
+
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars':Sidebar.getall(),
+        })
+
+        context.update(Category.get_navs())
+        return context
 
 
+class IndexView(CommonViewMixin, ListView):
+    queryset =  Post.latest_posts()
+    paginate_by = 5
+    context_object_name = "post_list"
+    template_name = 'blog/list.html'
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        context.update({
+            'category':category,
+        })
+        return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context = ({
+            'tag':tag,
+        })
+        return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+"""
 def post_list(request, category_id=None, tag_id=None):
     tag = None
     category = None
@@ -44,3 +98,4 @@ def post_detail(request, post_id):
     }
     context.update(Category.get_navs())
     return render(request, 'blog/detail.html',context=context)
+"""
